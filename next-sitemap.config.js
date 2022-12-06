8346612e51e638702a58d343ffc1370bf455fcf4
Surjith S M <surjithctly@gmail.com>
2022-12-06 19:13:47 +0530

    remove vercel url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl:
-    process.env.SITE_URL ||
-    process.env.VERCEL_URL ||
-    "https://stablo-pro.web3templates.com",
+    process.env.SITE_URL || "https://stablo-pro.web3templates.com",
   generateRobotsTxt: true // (optional)
   // ...other options
 };
